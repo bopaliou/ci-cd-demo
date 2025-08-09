386e164bf0da89491dd38f3fb02d0f6b27d2fc3f
Aliou Bop <bopaliou1994@gmail.com>
2025-08-09 14:26:27 +0000

    Mise à jour Dockerfile et app Flask pour CI/CD

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
 def greet():
-    return "Hello CI/CD World"
+    return "Hello CI/CD Flask!"
 
 if __name__ == "__main__":
-    print(greet())
+    app.run(host="0.0.0.0", port=5000)   
--- test_main.py
+++ test_main.py
@@ -1,4 +1,7 @@
-from main import greet
+from main import app
 
-def test_greet():
-    assert greet() == "Hello CI/CD World"
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello CI/CD Flask!" in response.data
